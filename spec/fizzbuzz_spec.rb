6976ba469cf34c51b5f82690c1b7b784cc424c0a
Anouska Hopkins <anouskahopkins@gmail.com>
2018-09-20 17:19:37 +0100

    adding test for not 3/5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,7 @@ describe 'fizzbuzz' do
   it 'return "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'return number when not passed 3 or 5' do
+    expect(fizzbuzz(8)).to eq 8
+  end
 end
