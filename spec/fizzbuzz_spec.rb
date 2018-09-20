52130231c06d537b7dae1d563967ed516304bbcf
Muna <mmhussein125@gmail.com>
2018-09-20 17:25:35 +0100

    adding final draft code for fizzbuzz_spec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,12 +1,21 @@
-require 'fizzbuzz'
+require_relative '../lib/fizzbuzz'
 describe 'fizzbuzz' do
-  it 'return "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'return "Fizz" when passed a multiple of 3' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
   end
-  it 'return "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+  it 'return "Buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
   end
-  it 'return number when not passed 3 or 5' do
-    expect(fizzbuzz(8)).to eq 8
+  it 'return "Fizz" when passed a multiple of 3' do
+    expect(fizzbuzz(6)).to eq 'Fizz'
+  end
+  it 'return "Buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(10)).to eq 'Buzz'
+  end
+  it 'return number when passed a number which isn\'t a multiple of 3 or 5' do
+    expect(fizzbuzz(7)).to eq 7
+  end
+  it 'return "FizzBuzz when passed a multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq "FizzBuzz"
   end
 end
